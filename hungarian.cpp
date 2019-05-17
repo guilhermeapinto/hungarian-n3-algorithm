6be8718d60388c8ef7b29e0b7ca842542fce50ea
Guilherme Albuquerque Pinto <guilherme.pinto@gmail.com>
2019-05-16 23:22:11 -0300

    modularisations and macros for improved readability

--- hungarian.cpp
+++ hungarian.cpp
@@ -42,14 +42,16 @@
 #include <vector>
 
 typedef long long ll;
-
 using namespace std;
 
 int N;
 vector<vector<ll>> c;
 vector<int> mate_V,mate_U,nhbor,parent;
-vector<ll> alpha,beta,slack;
+vector<ll> alpha,beta,slack,min_col;
 vector<bool> label_V,label_U;
+
+#define for_unlabelled_u  for ( int u = 0; u < N; u++ ) if ( not label_U[u] )
+#define for_unmatched_v   for ( int v = 0; v < N; v++ ) if ( mate_V[v] == -1 )
 
 void augment( int v, int exposed ) {
 
@@ -65,64 +67,101 @@ void augment( int v, int exposed ) {
 
 void update_slack( int v ) {
   
-  for ( int u = 0; u < N; u++ ) {
-    if ( not label_U[u] ) {
-      ll bound = c[v][u]-alpha[v]-beta[u];
-      if ( (bound >= 0LL) and (bound < slack[u]) ) {
-	slack[u] = bound;
-	nhbor[u] = v;
-      }
+  for_unlabelled_u {
+    ll bound = c[v][u]-alpha[v]-beta[u];
+    if ( 0LL <= bound and bound < slack[u] ) {
+      slack[u] = bound;
+      nhbor[u] = v;
     }
   }
+  
+}
+
+void initialize_alpha_beta() {
+
+  mate_V = vector<int>(N,-1);
+  mate_U = vector<int>(N,-1);
+  alpha = vector<ll>(N,0LL);
+  beta = vector<ll>(N);
+  for ( int u = 0; u < N; u++ )
+    beta[u] = min_col[u];
+  
+}
+
+ll update_alpha_beta() {
+  
+  ll theta = numeric_limits<ll>::max();
+
+  for_unlabelled_u {
+    theta = min( theta, slack[u] );
+  }
+  
+  // integrality is ensured
+  theta /= 2LL;
+  
+  for ( int k = 0; k < N; k++ ) {
+    if ( label_V[k] ) alpha[k] += theta;
+    else alpha[k] -= theta;
+    if ( label_U[k] ) beta[k] -= theta;
+    else beta[k] += theta;
+  }
+  
+  return theta;
+}
+
+void initialize_search() {
+
+  nhbor = vector<int>(N,-1);
+  parent = vector<int>(N,-1);
+  slack = vector<ll>(N,numeric_limits<ll>::max());
+  label_V = vector<bool>(N,false);
+  label_U = vector<bool>(N,false);
   
 }
 
 void search() {
 
   for ( int i = 0; i < N; i++ ) {
-    
-    ll theta = numeric_limits<ll>::max();
-
-    for ( int u = 0; u < N; u++ )
-      if ( not label_U[u] )
-	theta = min( theta, slack[u] );
-
-    // theta will be even before the division
-    // as the input is multiplied by 2
-    theta /= 2LL;
-
-    if ( theta > 0LL ) {
-      for ( int k = 0; k < N; k++ ) {
-	if ( label_V[k] ) alpha[k] += theta;
-	else alpha[k] -= theta;
-	if ( label_U[k] ) beta[k] -= theta;
-	else beta[k] += theta;
-      }
-    }
-    
+    ll theta = update_alpha_beta();
+       
     vector<int> admissibles = vector<int>();
 
-    for ( int u = 0; u < N; u++ ) {
-      if ( not label_U[u] ) {
-	slack[u] -= 2LL*theta;
-	if ( slack[u] == 0LL ) {
-	  if ( mate_U[u] == -1 ) {
-	    augment( nhbor[u], u );
-	    return;
-	  } else { 
-	    admissibles.push_back( u );
-	  }
+    for_unlabelled_u {
+      slack[u] -= 2LL*theta;
+      if ( slack[u] == 0LL ) {
+	if ( mate_U[u] == -1 ) {
+	  augment( nhbor[u], u );
+	  return;
+	} else { 
+	  admissibles.push_back( u );
 	}
       }
     }
     
     for ( int u: admissibles ) {
-      label_U[u] = label_V[mate_U[u]] = true;
+      label_U[u] = true;
+      label_V[mate_U[u]] = true;
       parent[mate_U[u]] = nhbor[u];
       update_slack( mate_U[u] );
-    }
-    
+    }    
   }
+  
+}
+
+void read_input() {
+  
+  cin >> N;
+  
+  c = vector<vector<ll>>(N,vector<ll>(N));
+  min_col = vector<ll>(N,numeric_limits<ll>::max());
+  
+  for ( int v = 0; v < N; v++ )
+    for ( int u = 0; u < N; u++ ) {
+      cin >> c[v][u];
+      // multiply by 2LL to ensure integrality
+      c[v][u] *= 2LL;
+      min_col[u] = min( min_col[u], c[v][u] );
+    }
   
 }
 
@@ -130,55 +169,25 @@ int main() {
 
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  
-  cin >> N;
-  
-  c = vector<vector<ll>>(N,vector<ll>(N));
-  
-  vector<ll> min_col = vector<ll>(N,numeric_limits<ll>::max());
-  
-  // input
-  for ( int v = 0; v < N; v++ )
-    for ( int u = 0; u < N; u++ ) {
-      cin >> c[v][u];
-      // multiply by 2
-      c[v][u] *= 2LL;
-      min_col[u] = min( min_col[u], c[v][u] );
+
+  read_input();
+  initialize_alpha_beta();
+
+  for ( int i = 0; i < N; i++ ) {
+    initialize_search();
+    
+    for_unmatched_v {
+      label_V[v] = true;
+      update_slack( v );
     }
-  
-  mate_V = vector<int>(N,-1);
-  mate_U = vector<int>(N,-1);
-  alpha = vector<ll>(N,0LL);
-  beta = vector<ll>(N);
-  for ( int u = 0; u < N; u++ )
-    beta[u] = min_col[u];
-
-  ///////////////////////////////////////////  
-  // main cycle of the Hungarian Algorithm //
-  ///////////////////////////////////////////  
-
-  for ( int s = 0; s < N; s++ ) {
-    nhbor = vector<int>(N,-1);
-    parent = vector<int>(N,-1);
-    slack = vector<ll>(N,numeric_limits<ll>::max());
-    label_V = vector<bool>(N,false);
-    label_U = vector<bool>(N,false);
-
-    for ( int v = 0; v < N; v++ )
-      if ( mate_V[v] == -1 ){
-        label_V[v] = true;
-	update_slack( v );
-      }
-
+    
     search();
   }
-  
-  //////////////////////////////////////////////  
-  
+    
   ll min_cost = 0LL;
-  for ( int v = 0; v < N; v++ ) {
-    min_cost += c[v][mate_V[v]];
-  }
+  for ( int i = 0; i < N; i++ )
+    min_cost += alpha[i]+beta[i];
+
   cout << min_cost/2LL << endl;
   
   return 0;
